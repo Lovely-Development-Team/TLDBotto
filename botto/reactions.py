d9bec01c7705ee367e4c05464275fe67b638c979
David Stephens <git@dwrs.org>
2021-05-27 20:15:33 +0100

    Remove most of the Motto processing

--- botto/reactions.py
+++ botto/reactions.py
@@ -4,14 +4,14 @@ import random
 
 from discord import Message
 
-import MottoBotto
+import tld_botto
 from food import SpecialAction
 
 log = logging.getLogger("MottoBotto").getChild("reactions")
 log.setLevel(logging.DEBUG)
 
 
-async def skynet_prevention(botto: MottoBotto, message: Message):
+async def skynet_prevention(botto: tld_botto, message: Message):
     log.info(f"{message.author} attempted to activate Skynet!")
     await message.add_reaction(botto.config["reactions"]["reject"])
     await message.add_reaction(botto.config["reactions"]["skynet"])
@@ -19,27 +19,27 @@ async def skynet_prevention(botto: MottoBotto, message: Message):
         await message.reply("Skynet prevention")
 
 
-async def snail(botto: MottoBotto, message: Message):
+async def snail(botto: tld_botto, message: Message):
     log.info(f"Snail from: {message.author}")
     await message.add_reaction("🐌")
 
 
-async def poke(botto: MottoBotto, message: Message):
+async def poke(botto: tld_botto, message: Message):
     log.info(f"Poke from: {message.author}")
     await message.add_reaction(random.choice(botto.config["reactions"]["poke"]))
 
 
-async def love(botto: MottoBotto, message: Message):
+async def love(botto: tld_botto, message: Message):
     log.info(f"Apology/love from: {message.author}")
     await message.add_reaction(random.choice(botto.config["reactions"]["love"]))
 
 
-async def hug(botto: MottoBotto, message: Message):
+async def hug(botto: tld_botto, message: Message):
     log.info(f"Hug from: {message.author}")
     await message.add_reaction(random.choice(botto.config["reactions"]["hug"]))
 
 
-async def party(botto: MottoBotto, message: Message):
+async def party(botto: tld_botto, message: Message):
     log.info(f"Party from: {message.author}")
     tasks = []
     for _ in range(5):
@@ -49,7 +49,7 @@ async def party(botto: MottoBotto, message: Message):
     await asyncio.wait(tasks)
 
 
-async def food(botto: MottoBotto, message: Message, food_item: str):
+async def food(botto: tld_botto, message: Message, food_item: str):
     reactions = botto.regexes.food.lookup[food_item]
     for reaction in reactions:
         if reaction == SpecialAction.echo:
@@ -60,11 +60,11 @@ async def food(botto: MottoBotto, message: Message, food_item: str):
             await message.add_reaction(reaction)
 
 
-async def unrecognised_food(botto: MottoBotto, message: Message):
+async def unrecognised_food(botto: tld_botto, message: Message):
     await message.add_reaction("😵")
 
 
-async def not_reply(botto: MottoBotto, message: Message):
+async def not_reply(botto: tld_botto, message: Message):
     log.info(
         f"Suggestion from {message.author} was not a reply (Message ID {message.id})"
     )
@@ -73,32 +73,32 @@ async def not_reply(botto: MottoBotto, message: Message):
         await message.reply("I see no motto!")
 
 
-async def fishing(botto: MottoBotto, message: Message):
+async def fishing(botto: tld_botto, message: Message):
     log.info(f"Motto fishing from: {message.author}")
     await message.add_reaction(botto.config["reactions"]["reject"])
     await message.add_reaction(botto.config["reactions"]["fishing"])
 
 
-async def invalid(botto: MottoBotto, message: Message):
+async def invalid(botto: tld_botto, message: Message):
     log.info(f"Motto from {message.author} is invalid according to rules.")
     await message.add_reaction(botto.config["reactions"]["reject"])
     await message.add_reaction(botto.config["reactions"]["invalid"])
 
 
-async def duplicate(botto: MottoBotto, message: Message):
+async def duplicate(botto: tld_botto, message: Message):
     log.debug("Ignoring motto, it's a duplicate.")
     await message.add_reaction(botto.config["reactions"]["repeat"])
     await message.remove_reaction(botto.config["reactions"]["pending"], botto.user)
 
 
-async def deleted(botto: MottoBotto, message: Message):
+async def deleted(botto: tld_botto, message: Message):
     log.debug("Ignoring motto, it's been deleted.")
     await message.add_reaction(botto.config["reactions"]["deleted"])
     await message.add_reaction(botto.config["reactions"]["reject"])
     await message.remove_reaction(botto.config["reactions"]["pending"], botto.user)
 
 
-async def stored(botto: MottoBotto, message: Message, motto_message: Message):
+async def stored(botto: tld_botto, message: Message, motto_message: Message):
     await message.remove_reaction(botto.config["reactions"]["pending"], botto.user)
     await message.add_reaction(botto.config["reactions"]["success"])
     if special_reactions := botto.config["special_reactions"].get(
@@ -111,37 +111,37 @@ async def stored(botto: MottoBotto, message: Message, motto_message: Message):
     log.debug("Reply sent")
 
 
-async def pending(botto: MottoBotto, message: Message, motto_message: Message):
+async def pending(botto: tld_botto, message: Message, motto_message: Message):
     await message.add_reaction(botto.config["reactions"]["pending"])
     log.debug("Reaction added")
 
 
-async def invalid_emoji(botto: MottoBotto, message: Message):
+async def invalid_emoji(botto: tld_botto, message: Message):
     log.info(f"Invalid emoji requested from {message.author}")
     await message.add_reaction(botto.config["reactions"]["invalid_emoji"])
 
 
-async def valid_emoji(botto: MottoBotto, message: Message):
+async def valid_emoji(botto: tld_botto, message: Message):
     log.info(f"Valid emoji requested from {message.author}")
     await message.add_reaction(botto.config["reactions"]["valid_emoji"])
 
 
-async def rule_1(botto: MottoBotto, message: Message):
+async def rule_1(botto: tld_botto, message: Message):
     for emoji in botto.config["reactions"]["rule_1"]:
         await message.add_reaction(emoji)
     log.info(f"Someone broke rule #1")
 
 
-async def favorite_band(botto: MottoBotto, message: Message):
+async def favorite_band(botto: tld_botto, message: Message):
     for letter in botto.config["reactions"]["favorite_band"]:
         await message.add_reaction(letter)
     log.info(f"Someone asked for favorite band")
 
 
-async def off_topic(botto: MottoBotto, message: Message):
+async def off_topic(botto: tld_botto, message: Message):
     await message.add_reaction(random.choice(botto.config["reactions"]["off_topic"]))
 
 
-async def unknown_dm(botto: MottoBotto, message: Message):
+async def unknown_dm(botto: tld_botto, message: Message):
     log.info(f"I don't know how to handle {message.content} from {message.author}")
     await message.add_reaction(botto.config["reactions"]["unknown"])
