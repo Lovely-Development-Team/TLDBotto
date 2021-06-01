c9448029eb0d406b9c9aeaae93939994cd1d4438
David Stephens <git@dwrs.org>
2021-06-01 23:29:10 +0100

    Ability to specify message for yelling

--- botto/tld_botto.py
+++ botto/tld_botto.py
@@ -402,5 +402,6 @@ You can DM me the following commands:
         """
         channel: discord.TextChannel = message.channel
         person = kwargs.get("person", "lovely person")
+        message = kwargs.get("text", "YOU SHOULD BE SLEEPING")
         async with channel.typing():
-            await channel.send(f"{person.upper()}, YOU SHOULD BE SLEEPING")
+            await channel.send(f"{person.upper()}, {message.lstrip().upper()}")
