61ccaad47fba72992abc419793be28a913b73eec
David Stephens <david.2.stephens@bt.com>
2021-06-30 15:08:59 +0100

    Move "off_topic" to pattern_reactions

--- botto/reactions.py
+++ botto/reactions.py
@@ -101,9 +101,6 @@ class Reactions:
             await message.add_reaction(letter)
         log.info(f"Someone asked for favorite band")
 
-    async def off_topic(self, message: Message):
-        await message.add_reaction(random.choice(self.config["reactions"]["off_topic"]))
-
     async def unknown_dm(self, message: Message):
         log.info(f"I don't know how to handle {message.content} from {message.author}")
         await message.add_reaction(self.config["reactions"]["unknown"])
