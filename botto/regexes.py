c7fd5e64a36ad81ce9f65c73e3b19dbb47007733
David Stephens <david.2.stephens@bt.com>
2021-06-30 15:31:16 +0100

    Reformat

--- botto/regexes.py
+++ botto/regexes.py
@@ -61,7 +61,8 @@ def compile_regexes(bot_user_id: str, config: dict) -> SuggestionRegexes:
     # Compile pattern reactions
     for key, triggers in config["pattern_reactions"].items():
         config["pattern_reactions"][key]["trigger"] = re.compile(
-            replace_bot_id(config["pattern_reactions"][key]["trigger"], self_id), re.IGNORECASE
+            replace_bot_id(config["pattern_reactions"][key]["trigger"], self_id),
+            re.IGNORECASE,
         )
 
     regexes = SuggestionRegexes(
