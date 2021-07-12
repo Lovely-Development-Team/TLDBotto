e11380358a717391dd85bfd4eca878f2a6f05d61
David Stephens <git@dwrs.org>
2021-07-12 12:00:04 +0100

    Recognise "I am also snail". Also moves the snail reaction into config.

--- botto/tld_botto.py
+++ botto/tld_botto.py
@@ -321,10 +321,6 @@ class TLDBotto(discord.Client):
             (lambda content: self.regexes.sorry.search(content), self.reactions.love),
             (lambda content: self.regexes.love.search(content), self.reactions.love),
             (lambda content: self.regexes.hug.search(content), self.reactions.hug),
-            (
-                lambda content: content.strip().lower() in ("i am 🐌", "i am snail"),
-                self.reactions.snail,
-            ),
         ]
 
     async def react(self, message):
