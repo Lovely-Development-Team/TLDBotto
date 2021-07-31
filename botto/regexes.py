1b257262226b0d32f386e198b0c046d3fa0d5216
David Stephens <git@dwrs.org>
2021-07-31 20:36:14 +0100

    Party harder when there are multiple exclamation marks.

--- botto/regexes.py
+++ botto/regexes.py
@@ -96,7 +96,7 @@ def compile_regexes(bot_user_id: str, config: dict) -> SuggestionRegexes:
         hug=re.compile(rf"Hugs? {self_id}|Gives {self_id} a?\s?hugs?", re.IGNORECASE),
         food=FoodLookups(self_id, config["food"]),
         party=re.compile(
-            rf"(?<!third)(?<!3rd)(?<!wrong)(?:^|\s)(?P<partyword>part(?:a*y|ies))(?:!|$)\s?",
+            rf"(?<!third)(?<!3rd)(?<!wrong)(?:^|\s)(?P<partyword>part(?:a*y|ies)(?:!+|$))\s?",
             re.IGNORECASE,
         ),
         patterns=PatternReactions(config["pattern_reactions"]),
