3197a22ffcd18e1743cf93653abef3579359863a
David Stephens <git@dwrs.org>
2021-08-24 12:46:05 +0100

    Fix converting 12pm or 12am to 24hr clock.

--- botto/date_helpers.py
+++ botto/date_helpers.py
@@ -6,3 +6,13 @@ from arrow import Arrow
 
 def is_naive(time: Union[datetime.datetime, Arrow]) -> bool:
     return time.tzinfo is None or time.tzinfo.utcoffset(time) is None
+
+
+def convert_24_hours(hours: int, is_pm: bool) -> int:
+    hours_is_12 = hours == 12
+    if is_pm and not hours_is_12:
+        return hours + 12
+    elif not is_pm and hours_is_12:
+        return 0
+    else:
+        return hours
