9b573023dc6db5bef1e75496f88634c945412181
Rosemary Orchard <16113535+RosemaryOrchard@users.noreply.github.com>
2022-01-20 00:15:44 +0000

    Some pasta with the copy

--- botto/models/meal_emoji.py
+++ botto/models/meal_emoji.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import relationship
 from botto.models import Base
 
 meal_time_emoji = Table(
-    "meal_time_text",
+    "meal_time_emoji",
     Base.metadata,
     Column("meal_time_id", ForeignKey("meal_time.id")),
-    Column("meal_text_id", ForeignKey("meal_text.id")),
+    Column("meal_emoji_id", ForeignKey("meal_emoji.id")),
 )
 
 
