c00d166857d1f2d6f7e9e7c689751eace42fac85
David Stephens <git@dwrs.org>
2023-07-23 12:15:23 +0100

    Add slash command for getting tester information

--- botto/clients/app_store_connect.py
+++ botto/clients/app_store_connect.py
@@ -1,6 +1,6 @@
+import logging
 from dataclasses import dataclass
-from datetime import datetime
-from typing import Optional
+from typing import Optional, Literal
 
 import aiohttp
 import arrow
@@ -13,12 +13,25 @@ from botto.storage.beta_testers.model import (
     ConfigError,
 )
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
 
 @dataclass
 class ApiKey:
     iss: str
     kid: str
     secret: str
+
+
+@dataclass(frozen=True, slots=True)
+class BetaTester:
+    id: str
+    email: str
+    first_name: str
+    last_name: Optional[str]
+    invite_type: Literal["EMAIL", "PUBLIC_LINK"]
+    beta_group_ids: list[str]
 
 
 class AppStoreConnectClient:
@@ -50,6 +63,65 @@ class AppStoreConnectClient:
 
     def make_auth_header(self, key_id) -> dict[str, str]:
         return {"Authorization": f"Bearer {self.create_token(key_id)}"}
+
+    async def find_beta_tester(
+        self, email: str, app: Optional[App] = None
+    ) -> list[BetaTester]:
+        async def run_command(key_id):
+            async with aiohttp.ClientSession() as session:
+                response = await session.get(
+                    "https://api.appstoreconnect.apple.com/v1/betaTesters",
+                    params={
+                        "filter[email]": email,
+                        "fields[betaTesters]": "betaGroups,email,firstName,lastName",
+                    },
+                    headers=self.make_auth_header(key_id),
+                    raise_for_status=True,
+                )
+                json = await response.json()
+                testers = []
+                if data := json.get("data"):
+                    for tester in data:
+                        attributes = tester.get("attributes", {})
+                        relationships = tester.get("relationships", {})
+                        beta_groups_link = (
+                            relationships.get("betaGroups", {})
+                            .get("links", {})
+                            .get("self")
+                        )
+                        ids_response = await (
+                            await session.get(
+                                beta_groups_link,
+                                headers=self.make_auth_header(key_id),
+                                raise_for_status=True,
+                            )
+                        ).json()
+                        beta_groups = [
+                            group["id"]
+                            for group in ids_response.get("data", [])
+                            if group.get("id") is not None
+                        ]
+                        testers.append(
+                            BetaTester(
+                                id=tester["id"],
+                                email=attributes.get("email"),
+                                first_name=attributes.get("firstName"),
+                                last_name=attributes.get("lastName"),
+                                invite_type=attributes.get("inviteType"),
+                                beta_group_ids=beta_groups,
+                            )
+                        )
+                return testers
+
+        testers = []
+        if app := app:
+            app_testers = await run_command(app.app_store_key_id)
+            testers.extend(app_testers)
+        else:
+            for key in self.config.keys():
+                app_testers = await run_command(key)
+                testers.extend(app_testers)
+        return testers
 
     async def create_beta_tester(
         self, app: App, email: str, first_name: Optional[str], last_name: Optional[str]
