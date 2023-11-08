fd6d1adfa8bdd1f3e7fc8f71f99cda864e88d507
Dom Chester <dom.chester@me.com>
2023-11-08 18:36:20 +0300

    Reduce logging level for discordpy

--- botto/__init__.py
+++ botto/__init__.py
@@ -2,7 +2,7 @@ import logging.config
 
 # Configure logging
 logging.config.fileConfig(fname="log.conf", disable_existing_loggers=False)
-logging.getLogger("discord").setLevel(logging.CRITICAL)
+logging.getLogger("discord").setLevel(logging.ERROR)
 logging.getLogger("discord.gateway").setLevel(logging.INFO)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("urllib").setLevel(logging.CRITICAL)
